04c6e068db4f504e36b8636679e6eb3b395f5491
Visionic10 <innocentclinton10@gmail.com>
2022-09-13 15:21:16 -0700

    My commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - prints _putchar, followed by a new line 
+ * main - prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	char cli [] = "_putchar";
+	char cli[] = "_putchar";
 
 	int c;
 	
